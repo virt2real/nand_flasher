0171983421fe138d700b40a11ec756a9709d4c1d
Gol <root@virt2real.(none)>
2014-02-05 08:35:51 -0500

    Fixed NAND settings

--- sdcard_flash/sdboot_flash_cfg.h
+++ sdcard_flash/sdboot_flash_cfg.h
@@ -1,4 +1,3 @@
-
 #define BLOCK 512
 #define KB 1024
 #define MB 0x100000
@@ -16,12 +15,14 @@
 // locations in NAND flash
 
 #define KERNEL_FLASH 0x400000
-#define ROOTFS_FLASH 0x600000
+//#define ROOTFS_FLASH 0x600000
+#define ROOTFS_FLASH 0x900000
 // 0x3e0000 - uboot env
+#define CONFIG_ENV_OFFSET            0x3C0000    /* environment starts here */
 
-#define UBL_CCS_ENTRY 0x20	// CCS UBL ublDM355-nand.bin
+#define UBL_CCS_ENTRY 0x20		// CCS UBL ublDM355-nand.bin
 #define UBL_CCS_MAGIC 0xE59F0124	// CCS UBL ublDM355-nand.bin first word
-#define UBL_GNU_ENTRY 0x100	// gcc UBL from DM35x_FlashAndBootUtils_1_10
+#define UBL_GNU_ENTRY 0x100		// gcc UBL from DM35x_FlashAndBootUtils_1_10
 #define UBL_GNU_MAGIC 0XEE190F31	// gcc UBL from DM35x_FlashAndBootUtils_1_10 first word
 
 // sizes
@@ -31,7 +32,8 @@
 //#define UBL_ENTRY UBL_GNU_ENTRY
 #define UBL_ENTRY UBL_CCS_ENTRY
 //#define UBOOT_SIZE (160*KB)
-#define UBOOT_SIZE (512*KB)
+//#define UBOOT_SIZE (512*KB)
+#define UBOOT_SIZE (320*KB)
 
 #define KERNEL_SIZE (3*MB)
 #define ROOTFS_SIZE (4*MB)
@@ -44,9 +46,8 @@
 #define UBL_SDC 	0x010000
 #define UBOOT_SDC	0x020000
 //#define KERNEL_SDC	0x060000
-#define KERNEL_SDC	0x080000
-
+//#define KERNEL_SDC	0x080000
+#define KERNEL_SDC	0x070000
 #define ROOTFS_SDC	0x400000
 
 #define MAGIC_NUMBER_VALID          (0xA1ACED00)
-
